cdd770d55f40d46c4723cb1d054b698c89006497
M7zain <zeynkara91@gmail.com>
2024-08-18 17:10:30 +0300

    Hero section responsive 	modified:   src/app/Sections/Hero.tsx

--- src/app/Sections/Hero.tsx
+++ src/app/Sections/Hero.tsx
@@ -5,16 +5,21 @@ import Button from '@/app/ui/Button'
 
 const Hero = () => {
   return (
-    <section className='flex p-10 items-center justify-center mt-10 mx-[15vw]' id='home'>
-      <div className='w-full text-balance  '>
-        <h1 className='text-6xl leading-relaxed font-rb font-bold text-oliveGreen'>Empower Your Projects with Collaborative Development</h1>
+    <section className='flex flex-col md:flex-row p-10 items-center justify-center  mt-40 md:mt-10 md:px-[15vw] ' id='home'>
+     
+      <div className='w-full text-balance flex flex-col items-center md:items-start'>
+        <h1 className=' text-4xl md:text-6xl leading-relaxed font-rb font-bold text-oliveGreen text-center md:text-left '>Empower Your Projects with Collaborative Development</h1>
        
-        <p className='text-xl leading-relaxed mt-14 font-rb font-light'>Join a community of skilled freelancers and bring your software ideas to life—efficiently and expertly.</p>
+        {/* mobile image */}
+        <Image src={heroImage} alt='coding infront of a pc holding a cup of coffee '  width={400} height={400} className='flex md:hidden my-10'/>
+
+        <p className='text-lg md:text-xl leading-relaxed mt-14 font-rb font-light '>Join a community of skilled freelancers and bring your software ideas to life—efficiently and expertly.</p>
       
         <Button buttonType='Get Started' className='mt-10' />
 
       </div>
-      <Image src={heroImage} alt='coding infront of a pc holding a cup of coffee '  width={750} height={750}/>
+      {/* Desktop image */}
+      <Image src={heroImage} alt='coding infront of a pc holding a cup of coffee '   width={750} height={750} className='hidden md:flex' />
     </section>
   )
 }
