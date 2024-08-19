461c22aa85c2c71eb2a3b421572806eaad27aed9
M7zain <zeynkara91@gmail.com>
2024-08-19 17:02:36 +0300

    optimized hero for desktop

--- src/app/Sections/Hero.tsx
+++ src/app/Sections/Hero.tsx
@@ -5,7 +5,7 @@ import Button from '@/app/ui/Button'
 
 const Hero = () => {
   return (
-    <section className='flex flex-col md:flex-row p-10 items-center justify-center   lg:px-[15vw]  min-h-screen md:min-h-0 ' id='home'>
+    <section className='flex flex-col md:flex-row p-10 items-center justify-center   lg:px-[15vw]  min-h-screen lg:min-h-full' id='home'>
      
       <div className='w-full text-balance flex flex-col items-center justify-center mt-10 sm:mt-20 md:mt-5 lg:mt-0 md:items-start'>
         <h1 className=' text-2xl md:text-4xl lg:text-6xl leading-normal lg:leading-relaxed font-rb font-bold text-oliveGreen text-center md:text-left '>Empower Your Projects with Collaborative Development</h1>
