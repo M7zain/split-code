5f2f12b8667361d8fae1e882b27a84219ededfbb
M7zain <zeynkara91@gmail.com>
2024-08-19 17:04:29 +0300

    Aceternity Ui dependencies installed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,14 @@
 import type { Config } from "tailwindcss";
 
+
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -30,6 +39,27 @@ const config: Config = {
     },
     
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+
+
+
+
+
 export default config;
