dc2804f0333cd6bcea501003eb599e28b131f9a2
M7zain <zeynkara91@gmail.com>
2024-08-19 15:38:04 +0300

    modified:   src/app/Sections/Hero.tsx

--- src/app/Sections/Hero.tsx
+++ src/app/Sections/Hero.tsx
@@ -5,10 +5,10 @@ import Button from '@/app/ui/Button'
 
 const Hero = () => {
   return (
-    <section className='flex flex-col md:flex-row p-10 items-center justify-center   md:px-[15vw]  min-h-screen md:min-h-0 ' id='home'>
+    <section className='flex flex-col md:flex-row p-10 items-center justify-center   lg:px-[15vw]  min-h-screen md:min-h-0 ' id='home'>
      
-      <div className='w-full text-balance flex flex-col items-center justify-center  md:items-start'>
-        <h1 className=' text-xl md:text-4xl lg:text-6xl leading-relaxed font-rb font-bold text-oliveGreen text-center md:text-left '>Empower Your Projects with Collaborative Development</h1>
+      <div className='w-full text-balance flex flex-col items-center justify-center mt-10 sm:mt-20 md:mt-5 lg:mt-0 md:items-start'>
+        <h1 className=' text-2xl md:text-4xl lg:text-6xl leading-normal lg:leading-relaxed font-rb font-bold text-oliveGreen text-center md:text-left '>Empower Your Projects with Collaborative Development</h1>
        
         {/* mobile image */}
         <Image src={heroImage} alt='coding infront of a pc holding a cup of coffee '  width={350} height={350} className='flex md:hidden'/>
@@ -19,7 +19,7 @@ const Hero = () => {
 
       </div>
       {/* Desktop image */}
-      <Image src={heroImage} alt='coding infront of a pc holding a cup of coffee '   width={750} height={750} className='hidden md:flex' />
+      <Image src={heroImage} alt='coding infront of a pc holding a cup of coffee '  width={600} height={600} className='hidden md:flex' />
     </section>
   )
 }
