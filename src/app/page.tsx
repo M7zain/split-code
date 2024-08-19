8a18265c763f669408fa9dc84d317f19a03c820e
M7zain <zeynkara91@gmail.com>
2024-08-19 17:03:55 +0300

    tracing beam added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Hero from "@/app/Sections/Hero";
 import About from "./Sections/About";
+import { TracingBeam } from "./ui/tracing-beam";
 
 export default function Home() {
-  return (
+  return (  
     <main>
-     
+        
         <Hero/>
         <About/>
-    </main>
+        
+
+      
+    </main> 
   );
 }
