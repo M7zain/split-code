eeba5f9821724efa945613979d04875b37b1f67b
M7zain <zeynkara91@gmail.com>
2024-08-20 23:29:22 +0300

    adding images for how it works section and creating a index file

---
+++ public/images/index.ts
@@ -0,0 +1,14 @@
+import cloud from './cloud.svg'
+import launch from './launch.svg'
+import tasks from './tasks.svg'
+import track from './track.svg'
+ 
+
+export{ 
+
+    cloud, 
+    launch, 
+    tasks, 
+    track, 
+    
+}
