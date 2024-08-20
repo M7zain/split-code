2cf5da78a0dde126af7f3d2ab02f26abfac1cb6d
M7zain <zeynkara91@gmail.com>
2024-08-20 23:30:04 +0300

    index file for constants

---
+++ src/constants/index.ts
@@ -0,0 +1,42 @@
+import { MdDescription } from "react-icons/md";
+import { cloud, launch, tasks ,track  } from "../../public/images";
+
+
+
+export const howLeft = [
+    { 
+        
+        imgURL: cloud, 
+        id: "2",
+        title: "Freelancers Split Tasks", 
+        description: "We match your project with skilled developers who specialize in each task, ensuring quality and efficiency", 
+
+    },
+    {
+                
+        imgURL: track, 
+        id: "4",
+        title: "Launch with Confidence", 
+        description: "Receive your completed project, fully optimized and ready for deployment.", 
+
+    }
+]
+
+export const howRight = [
+    { 
+        
+        imgURL: tasks, 
+        id: "1",
+        title: "Post Your Project", 
+        description: "Describe your project needs, set your budget, and let us connect you with top freelancers.", 
+
+    },
+    {
+                
+        imgURL: launch, 
+        id: "3",
+        title: "Track and Collaborate", 
+        description: "Use our platform to monitor progress, communicate with your team, and make adjustments in real-time.", 
+
+    },
+]
