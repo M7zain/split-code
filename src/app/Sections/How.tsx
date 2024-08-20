598cc73954288f073fc3c9e72c493cf01d8d4ccf
M7zain <zeynkara91@gmail.com>
2024-08-20 23:30:27 +0300

    mapped through howLeft and howRight to create the left and right sections of how it works

--- src/app/Sections/How.tsx
+++ src/app/Sections/How.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { TracingBeam } from '../ui/tracing-beam'
 import Image from 'next/image'
-import cloud from "../../../public/how/cloud.svg"
+import cloud from "../../../public/images/cloud.svg"
+
+import { howLeft, howRight } from '@/constants'
+
 const How = () => {
   return (
-<section className='flex flex-col items-center justify-center px-[15vw] mt-20 ' id='How'>
+<section className='flex flex-col items-center justify-center px-[15vw] lg:px-[30vw]  mt-20 ' id='How'>
         <h1 className='text-4xl lg:text-6xl  font-rb font-bold text-oliveGreen  '> 
           How Split Code Works? 
         </h1>
@@ -12,31 +15,40 @@ const How = () => {
         <div className='flex  mt-20'>
                 
                 {/* Left side */}
-                <div>
-                 <Image src={cloud} alt="cloud image"/>
+                <div >
+                  {
+                    howLeft.map((item) => (
 
 
-                                     {/* number of the step */}
-                <div className='flex flex-col items-end mt-10'>   
-                    {/* number of the step */}
-                    <div className='flex items-center justify-center bg-splitOrange p-1 rounded-full w-10 h-10'>
-                     <p className=' text-white text-center '>
-                        2
-                      </p>
-                     </div>
+                  <div key={item.id} className='flex flex-col items-end  '>
+                        <Image src={item.imgURL} alt="cloud image" />
+                        
+                        <div className='flex flex-col items-end my-20'>
+                        {/* number of the step */}
+                        <div className='flex items-center justify-center bg-splitOrange p-1 rounded-full w-10 h-10'>
+                          <p className=' text-white text-center '>
+                            {item.id}
+                          </p>
+                        </div>
 
-                     <h3 className='font-rb text-lg font-bold text-nowrap'>
-                        Freelancers Split Tasks
-                     </h3>
-                    <p className='font-rb text-sm text-right'>
-                    We match your project with skilled developers who specialize
-                    in each task, ensuring quality and efficiency
-                    </p>
+                        <h3 className='font-rb text-lg font-bold text-nowrap'>
+                          {item.title}
+                        </h3>
+                        <p className='font-rb text-sm text-right'>
+                          {item.description}
+                        </p>
+
+
+                        </div>
+                  </div>
+                    ))
+                  }
 
 
                 </div>
 
-                </div>
+
+                
                  
 
                  {/* the tracing beam */}
@@ -47,23 +59,30 @@ const How = () => {
                  {/* Right side  */}
 
                 <div>   
-                    {/* number of the step */}
+                  { 
+                    howRight.map((item) => ( 
+                      <div key={item.id}>
+                         {/* number of the step */}
                     <div className='flex items-center justify-center bg-splitOrange p-1 rounded-full w-10 h-10'>
                      <p className=' text-white text-center '>
-                        1
+                        {item.id}
                       </p>
                      </div>
 
                      <h3 className='font-rb text-lg font-bold text-nowrap'>
-                        Post Your Project
+                        {item.title}
                      </h3>
                     <p className='font-rb text-sm'>
                     Describe your project needs, set your budget, 
                     and let us connect you with top freelancers.
                     </p>
 
-                    <Image src={cloud} alt="cloud image" className='mt-10'/>
+                    <Image src={item.imgURL} alt="cloud image" className='my-20'/>
 
+                      </div>
+                    )) 
+                  }
+                   
 
                 </div>
 
