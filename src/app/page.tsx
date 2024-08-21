41c0a2b047f56d627039e47dc2a2d5d178a661bf
M7zain <zeynkara91@gmail.com>
2024-08-21 12:12:07 +0300

    added Why section to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/app/Sections/Hero";
 import About from "./Sections/About";
 import How from "./Sections/How";
+import Why from "./Sections/Why";
 
 export default function Home() {
   return (  
@@ -9,6 +10,7 @@ export default function Home() {
         <Hero/>
         <About/>
         <How/>
+        <Why/>
 
       
     </main> 
