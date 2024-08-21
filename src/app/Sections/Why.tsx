b31b6e8ca898ede90b1090bd2d14588882b4e84c
M7zain <zeynkara91@gmail.com>
2024-08-21 12:55:17 +0300

    created grid system for cards in why section

--- src/app/Sections/Why.tsx
+++ src/app/Sections/Why.tsx
@@ -4,18 +4,21 @@ import Card from '../ui/Card'
 
 const Why = () => {
   return (
-    <section className='flex flex-col    items-center    py-16 lg:px-[15vw] mt-20' id='About'>
+    <section className='flex flex-col items-center py-16 lg:px-[15vw] mt-20 px-[10vw] md: px-0' id='About'>
         
         <h1 className='text-4xl lg:text-6xl  font-rb font-bold text-oliveGreen  '> 
             Why <span className='text-splitOrange'>Split Code </span> ? 
         </h1>
 
-        { 
 
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-9 gap-x-20 mt-10'>
+        { 
           cardInfo.map((item) => ( 
-            <Card imgURL={item.imgURL} title={item.title} description={item.description}/> 
+            <Card key={item.id} imgURL={item.imgURL} title={item.title} description={item.description}/> 
           ))
         }
+      </div>
+
 
     
 
