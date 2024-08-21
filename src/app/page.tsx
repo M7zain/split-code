80116a39c63f4c629176e3b24466e983de1736fc
M7zain <zeynkara91@gmail.com>
2024-08-21 21:36:08 +0300

    added ready and footer sections to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/app/Sections/Hero";
 import About from "./Sections/About";
 import How from "./Sections/How";
 import Why from "./Sections/Why";
+import Ready from "./Sections/Ready";
+import Footer from "./Sections/Footer";
 
 export default function Home() {
   return (  
@@ -11,7 +13,8 @@ export default function Home() {
         <About/>
         <How/>
         <Why/>
-
+        <Ready/>
+        <Footer/>
       
     </main> 
   );
