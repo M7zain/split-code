d61e8ddaf181a7e2f7ad30947d52c53694ac1e99
M7zain <zeynkara91@gmail.com>
2024-08-21 21:34:42 +0300

    fixed padding to md

--- src/app/Sections/Why.tsx
+++ src/app/Sections/Why.tsx
@@ -4,7 +4,7 @@ import Card from '../ui/Card'
 
 const Why = () => {
   return (
-    <section className='flex flex-col items-center py-16 lg:px-[15vw] mt-20 px-[10vw] md: px-0' id='About'>
+    <section className='flex flex-col items-center py-16 lg:px-[15vw] mt-20 px-[10vw] md:px-0' id='About'>
         
         <h1 className='text-4xl lg:text-6xl  font-rb font-bold text-oliveGreen  '> 
             Why <span className='text-splitOrange'>Split Code </span> ? 
