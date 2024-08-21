1bb67d318ad44a969eb3594a830f56d8aeff1e06
M7zain <zeynkara91@gmail.com>
2024-08-21 12:55:42 +0300

    drop shadow to config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,13 @@ const config: Config = {
       '2xl': '1536px',
     },
     extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
       colors: {
         splitOrange: "#FB8500",
         oliveGreen: "#013333",
