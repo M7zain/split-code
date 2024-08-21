9430dcbbf09d65941563280f68770f9d248eecee
M7zain <zeynkara91@gmail.com>
2024-08-21 12:54:41 +0300

    finished card component

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Card = ({imgURL , title , description} : {imgURL :string , title:string, description:string}) => {
+const Card = ({ imgURL, title, description }: { imgURL: string; title: string; description: string }) => {
   return (
-    <div>
-        <Image src={imgURL} alt={title}/> 
-        <h4>{title}</h4>
-        <p>
-            {description}
-        </p>
+    <div className="border-2 border-splitOrange rounded-xl my-10 p-5 max-w-96 h-60 shadow-lg">
+      <div className="h-full flex flex-col justify-evenly ">
+        <div>
+          <Image src={imgURL} alt={title} />
+        </div>
+        <div>
+          <h4 className="text-lg font-bold font-rb">{title}</h4>
+          <p className="text-sm text-gray-700 font-rb font-light">{description}</p>
+        </div>
+      </div>
     </div>
   )
 }
