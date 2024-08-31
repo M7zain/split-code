a8f3ec8ee886ba96d7d7e950c66b11bfb671efc8
M7zain <zeynkara91@gmail.com>
2024-08-31 11:46:26 +0300

    added navbar to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import How from "./Sections/How";
 import Why from "./Sections/Why";
 import Ready from "./Sections/Ready";
 import Footer from "./Sections/Footer";
+import Navbar from "./ui/Navbar";
 
 export default function Home() {
   return (  
     <main>
-        
+        <Navbar/>
         <Hero/>
         <About/>
         <How/>
