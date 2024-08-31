b78170d25fb1ca316cffac0f9774b11511678fd9
M7zain <zeynkara91@gmail.com>
2024-08-31 13:49:01 +0300

    feed page and sign in redirection

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,23 +1,30 @@
-import React, { ReactElement } from 'react'
-import Layout from './layout'
+'use client';
+import React, { ReactElement, useEffect } from 'react';
+import Layout from './layout';
+import { useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
 
+const Feed = () => {
+  const { isSignedIn, isLoaded } = useUser(); // Check if user state is loaded
+  const router = useRouter();
 
-  const Feed = () => {
-    return (
-      <div>feed</div>
-    )
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      // Redirect to login if not signed in
+      router.push(`/login?redirect=/feed`);
+    }
+  }, [isLoaded, isSignedIn, router]);
+
+  if (!isLoaded || !isSignedIn) {
+    // Optionally, show a loading spinner while checking auth status
+    return <div>Loading...</div>;
   }
 
+  return <div>feed</div>;
+};
 
 Feed.getLayout = function getLayout(page: ReactElement) {
-    return (
-      <Layout>
-        {page}
-      </Layout>
-    )
-  }
-   
+  return <Layout>{page}</Layout>;
+};
 
-export default Feed
-
-
+export default Feed;
