6bc084eebf56224357b05bf3d692e11e2c658b4a
M7zain <zeynkara91@gmail.com>
2024-09-15 13:07:26 +0300

    fixed post layout on mobile

--- src/app/ui/feed/Post.tsx
+++ src/app/ui/feed/Post.tsx
@@ -10,7 +10,7 @@ const Post = ({date , title, position, description , difficulty} : { date : stri
   
   return (
     <>
-    <div className='flex justify-start md:justify-between '>
+    <div className='flex justify-between '>
     <div className='flex flex-col max-w-md text-balance'>
         <p className='font-rb text-gray-400 text-sm md:text-lg '>
            {date}
