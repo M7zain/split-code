204af6ce67b7d6633b4d0aefc573d05aa7e0ff95
M7zain <zeynkara91@gmail.com>
2024-09-16 11:47:01 +0300

    tab highlighting added

--- src/app/ui/feed/SectionsNav.tsx
+++ src/app/ui/feed/SectionsNav.tsx
@@ -2,11 +2,13 @@
 import logo from "../../../../public/logo.svg"
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { CiBellOn } from "react-icons/ci";
 
 const SectionsNav = () => {
 
     const router = useRouter(); 
+    const [tab , setTab] = useState(1); 
   return (
        
     <div className="p-6 bg-white sticky top-0 left-0 right-0 z-20">
@@ -21,9 +23,14 @@ const SectionsNav = () => {
       <h2 className="font-rb text-[24px] text-oliveGreen font-normal">Splits For You</h2>
 
       <div className="flex flex-row justify-between md:justify-start space-x-0 md:space-x-7 font-rb text-sm md:text-lg mt-4">
-        <button onClick={() => router.push("/feed/most-recent")}>Most Recent</button>
-        <button onClick={() => router.push("/feed/best-match")}>Best Matches</button>
-        <button onClick={() => router.push("/feed/saved-splits")}>Saved Splits</button>
+        <button className={`${tab === 1 ? "text-splitOrange" : "text-oliveGreen"}`}onClick={() => {
+                                router.push("/feed/most-recent"); 
+                                setTab(1);
+                                }}>Most Recent</button>
+        <button className={`${tab === 2 ? "text-splitOrange" : "text-oliveGreen"}`} onClick={() =>{ router.push("/feed/best-match")
+          setTab(2);}
+        }>Best Matches</button>
+        <button className={`${tab === 3 ? "text-splitOrange" : "text-oliveGreen"}`} onClick={() => {router.push("/feed/saved-splits"); setTab(3);}}>Saved Splits</button>
       </div>
 
       <hr className="h-px mt-3 bg-oliveGreen border-0"/>
