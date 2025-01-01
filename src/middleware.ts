01883de9951edb3e9d58bf9aeed3b8f5d45bcb52
M7zain <zeynkara91@gmail.com>
2025-01-01 23:27:46 +0300

    re configured middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,12 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-        // Exclude the /api/webhooks route from Clerk authentication
-        '/api/webhooks(.*)',  // This will make the webhook route public
+    // Exclude the /api/webhooks route from Clerk authentication
+    '/api/webhooks/(.*)',  // This will make the webhook route public
     
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
